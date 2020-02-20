18b6ec05ad6e2ca441412f559aa7615ca9548947
spiduso <hoang.dusan@gmail.com>
2020-02-20 20:56:03 +0100

    update

--- Program.cs
+++ Program.cs
@@ -22,105 +22,105 @@ namespace GoogleHascode
 	}
 
 
-	class Program
-	{
-		public static void PrintOutput(List<Library> chosen_libraries)
-		{
-			using (var sw = new StreamWriter("output.txt"))
-			{
-				sw.WriteLine(chosen_libraries.Count);
-				for (int i = 0; i < chosen_libraries.Count; i++)
-				{
-					sw.WriteLine("{0} {1}", chosen_libraries[i].id, chosen_libraries[i].num_of_books);
+    class Program
+    {
+        public static void PrintOutput(List<Library> chosen_libraries)
+        {
+            using (var sw = new StreamWriter("output.txt"))
+            {
+                sw.WriteLine(chosen_libraries.Count);
+                for (int i = 0; i < chosen_libraries.Count; i++)
+                {
+                    sw.WriteLine("{0} {1}", chosen_libraries[i].id, chosen_libraries[i].num_of_books);
 
-					int j = 0;
-					for (; j < chosen_libraries[i].num_of_books - 1; j++)
-					{
-						sw.Write("{0} ", chosen_libraries[i].books[j]);
-					}
-					sw.WriteLine("{0}", chosen_libraries[i].books[j]);
-				}
-			}
-		}
+                    int j = 0;
+                    for (; j < chosen_libraries[i].num_of_books - 1; j++)
+                    {
+                        sw.Write("{0} ", chosen_libraries[i].books[j]);
+                    }
+                    sw.WriteLine("{0}", chosen_libraries[i].books[j]);
+                }
+            }
+        }
 
-		public static int ReadInteger(ref StreamReader sr)
-		{
-			int number = 0;
-			int digit = sr.Read();
-			while ((digit >= 48) && (digit <= 57))
-			{
-				number = number * 10 + digit - 48;
-				digit = sr.Read();
-			}
-			return (number);
-		}
-
-
-		static void Main(string[] args)
-		{
+        public static int ReadInteger(ref StreamReader sr)
+        {
+            int number = 0;
+            int digit = sr.Read();
+            while ((digit >= 48) && (digit <= 57))
+            {
+                number = number * 10 + digit - 48;
+                digit = sr.Read();
+            }
+            return (number);
+        }
 
 
-			int number_of_books;
-			int number_of_libraries;
-			int number_of_days;
-			List<int> books = new List<int>();
-			List<Library> libraries = new List<Library>();
-
-			#region READING INPUT
-			//StreamReader sr = new StreamReader("a_example.txt");
-			//StreamReader sr = new StreamReader("b_read_on.txt");
-			//StreamReader sr = new StreamReader("c_incunabula.txt");
-			//StreamReader sr = new StreamReader("d_tough_choices.txt");
-			//StreamReader sr = new StreamReader("e_so_many_books.txt");
-			StreamReader sr = new StreamReader("f_libraries_of_the_world.txt");
-
-			//reading first line
-			string line = sr.ReadLine();
-			string[] nums_s = line.Split(' ');
-
-			number_of_books = int.Parse(nums_s[0]);
-			number_of_libraries = int.Parse(nums_s[1]);
-			number_of_days = int.Parse(nums_s[2]);
-
-			//reading second line
-			for (int i = 0; i < number_of_books; i++)
-			{
-				//ReadInteger(sr);
-
-				books.Add(ReadInteger(ref sr));
-
-			}
+        static void Main(string[] args)
+        {
 
 
-			//reading two lines for libraries
-			{
-				long score = 0;
-				for (int i = 0; i < number_of_libraries; i++)
-				{
-					Library lib = new Library();
-					lib.id = i;
-					line = sr.ReadLine();
-					nums_s = line.Split(' ');
+            int number_of_books;
+            int number_of_libraries;
+            int number_of_days;
+            List<int> books = new List<int>();
+            List<Library> libraries = new List<Library>();
 
-					lib.num_of_books = int.Parse(nums_s[0]);
-					lib.scann_time = int.Parse(nums_s[1]);
-					lib.scans_per_day = int.Parse(nums_s[2]);
+            #region READING INPUT
+            //StreamReader sr = new StreamReader("a_example.txt");
+            //StreamReader sr = new StreamReader("b_read_on.txt");
+            //StreamReader sr = new StreamReader("c_incunabula.txt");
+            //StreamReader sr = new StreamReader("d_tough_choices.txt");
+            //StreamReader sr = new StreamReader("e_so_many_books.txt");
+            StreamReader sr = new StreamReader("f_libraries_of_the_world.txt");
+
+            //reading first line
+            string line = sr.ReadLine();
+            string[] nums_s = line.Split(' ');
+
+            number_of_books = int.Parse(nums_s[0]);
+            number_of_libraries = int.Parse(nums_s[1]);
+            number_of_days = int.Parse(nums_s[2]);
+
+            //reading second line
+            for (int i = 0; i < number_of_books; i++)
+            {
+                //ReadInteger(sr);
+
+                books.Add(ReadInteger(ref sr));
+
+            }
 
 
-					if (!(lib.scann_time > number_of_days)) //doba nahrání knihovny je delší jak počet možných dnů
-					{
-						//přidám id knížek do knihovny
-						for (int j = 0; j < lib.num_of_books; j++)
-						{
-							int id = ReadInteger(ref sr);
-							score += books[id];
-							lib.books.Add(id);
-						}
-						lib.score = (score / (decimal)lib.scann_time);
-					}
-					libraries.Add(lib);
-				}
-			}
+            //reading two lines for libraries
+            {
+                long score = 0;
+                for (int i = 0; i < number_of_libraries; i++)
+                {
+                    Library lib = new Library();
+                    lib.id = i;
+                    line = sr.ReadLine();
+                    nums_s = line.Split(' ');
+
+                    lib.num_of_books = int.Parse(nums_s[0]);
+                    lib.scann_time = int.Parse(nums_s[1]);
+                    lib.scans_per_day = int.Parse(nums_s[2]);
+
+
+                    if (!(lib.scann_time > number_of_days)) //doba nahrání knihovny je delší jak počet možných dnů
+                    {
+                        //přidám id knížek do knihovny
+                        for (int j = 0; j < lib.num_of_books; j++)
+                        {
+                            int id = ReadInteger(ref sr);
+                            score += books[id];
+                            lib.books.Add(id);
+                        }
+                        lib.score = (score / (decimal)lib.scann_time);
+                    }
+                    libraries.Add(lib);
+                }
+            }
             #endregion
 
             const int ScoreOpt = 1;
@@ -136,6 +136,11 @@ namespace GoogleHascode
             // Get Best result
 
             //Backtrack and recieve libraries
+            //TODO CHANGE BACKTRACK PARAMETERS!!!!!!
+            int X = 5;
+            int Y = 7;
+            List<Library> used_libraries = new List<Library>();
+            used_libraries = BackTrack(bag, X, Y, used_libraries);
 
             //for (; ; );
             PrintOutput(libraries);
@@ -143,15 +148,40 @@ namespace GoogleHascode
 
         #region Algorithm
 
+        static List<Library> BackTrack(Cell[,] bag, int X, int Y, List<Library> used_libraries)
+        {
+            if (X == 0 || Y == 0)
+                return used_libraries;
+
+            if (bag[X,Y].IsLibraryUsed)
+                used_libraries.Add(used_libraries[X]);
+
+            return BackTrack(bag, bag[X, Y].PrevCellX, bag[X, Y].PrevCellY, used_libraries);
+        }
+
+        static void CalculateCell(Cell[,] bag, List<Library> libraries, int X, int Y)
+        {
+            // X is Libraries Y is score
+            if (bag[X - 1, Y].TotalTime < bag[X - 1, Y - (int)libraries[X - 1].score].TotalTime)
+            {
+                bag[X, Y] = new Cell(X - 1, Y, bag[X - 1, Y].TotalTime, false);
+            }
+            else
+            {
+                bag[X, Y] = new Cell(X - 1, Y - (int)libraries[X - 1].score, bag[X - 1, Y].TotalTime, true);
+            }
+        }
+
+
         struct Cell
         {
-            bool IsLibraryUsed;
+            public bool IsLibraryUsed;
 
-            int PrevCellX { get; }
+            public int PrevCellX { get; }
 
-            int PrevCellY { get; }
+            public int PrevCellY { get; }
 
-            int TotalTime { get; }
+            public int TotalTime { get; }
 
             public Cell(int PrevCellX, int PrevCellY, int TotalTime, bool used)
             {
